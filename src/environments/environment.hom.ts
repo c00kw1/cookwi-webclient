59eab7f12d9013e678e6f6e1a04448c6596e38bd
gjdass <gustave@dassonville.io>
2020-05-21 15:42:15 +0200

    PR pipeline in Jenkins should work
    _ disable artifact generation for PR

--- src/environments/environment.hom.ts
+++ src/environments/environment.hom.ts
@@ -4,12 +4,12 @@ export const environment = {
     dev: false,
     api: {
         scheme: "https",
-        hostname: "api.hom-1.cookwi.com",
+        hostname: "api.hom.cookwi.com",
         port: 443,
         prefix: "api"
     },
     sso: {
-        issuer: "https://sso.hom-1.cookwi.com",
+        issuer: "https://sso.hom.cookwi.com",
         clientId: "cf1f02a5-664f-4663-bb6f-407fb05f8bec"
     }
 };
